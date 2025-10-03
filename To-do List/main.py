cc10b65bd41a581ee7163b185587d546857aa193
Rey <reytendy@gmail.com>
2025-10-03 17:16:11 +0700

    membuat fitur baru : hapus aktivitas

--- To-do List/main.py
+++ To-do List/main.py
@@ -33,7 +33,33 @@ while True :
         data_aktvitas.append((input_aktivtas,input_jam))
         print('Aktivitas berhasil ditambahkan')
         
+    elif input_user ==2 :
         
+        if not data_aktvitas :
+            continue
+        
+        print('-----DAFTAR AKTIVITAS-----')
+        
+        for i,(aktivitas,jam) in enumerate(data_aktvitas) :
+            print(f'{i+1}. Aktivitas : {aktivitas}\nJam : {jam}')
+            
+        try :
+            
+            nomor_remove = int(input('Input no aktivitas yang akan dihapus :  '))
+            
+            if 1 <= nomer_remove <= len(data_aktvitas) :
+                
+                del data_aktvitas[nomor_remove-1]
+                print('Aktivitas berhasil dihapus.')
+                
+            else : 
+                
+                print('Aktvitas tidak valid.')
+                
+        except ValueError :
+            
+            print('Input tidak valid. gunakan angka yang tertera.')
+            
         
         
                 
